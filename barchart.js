de53fbed7f886ff89b28f969e8514a50f12a215f
javelinsman <javelinsman@snu.ac.kr>
2018-11-05 16:56:22 +0900

    spec -> graph working

--- barchart.js
+++ barchart.js
@@ -1,39 +1,55 @@
 function translate(x, y){
     return "translate(" + x + "," + y + ")"
 }
-let data = [
-    {
-        "type": "stacked_bar",
-        "stacks": [
-            {"value": 3, "color": "red"},
-            {"value": 5, "color": "blue"},
-            {"value": 2, "color": "green"}
-        ]
-    },
-    {
-        "type": "bar",
-        "bar": {"value": 6, "color": "yellow"}
-    },
-    {
-        "type": "stacked_bar",
-        "stacks": [
-            {"value": 5, "color": "red"},
-            {"value": 6, "color": "blue"},
-            {"value": 7, "color": "green"}
-        ]
-    },
-    {
-        "type": "stacked_bar",
-        "stacks": [
-            {"value": 3, "color": "purple"},
-            {"value": 2, "color": "cyan"},
-        ]
-    },
-    {
-        "type": "bar",
-        "bar": {"value": 7, "color": "blue"}
+
+/*
+{"type": "stacked_bar", "stacks": [{"value": 5, "color": "yellow"}, ...]}
+
+{"type": "bar", "bar": {"value": 5, "color": "yellow"}}
+
+*/
+
+let color_AB = "rgb(235, 160, 212)",
+    color_A = "rgb(99, 160, 203)",
+    color_B = "rgb(128, 128, 128)",
+    color_O = "rgb(188, 131, 196)"
+
+let barchart_spec = {
+    //"orientation": "horizontal",
+    "marks": [
+        {
+            "type": "stacked_bar",
+            "key": "Korea",
+            "stacks": [
+                {"value": 3, "color": color_A},
+                {"value": 5, "color": color_B},
+                {"value": 2, "color": color_O},
+                {"value": 6, "color": color_AB}
+            ]
+        },
+        {
+            "type": "stacked_bar",
+            "key": "Japan",
+            "stacks": [
+                {"value": 5, "color": color_A},
+                {"value": 6, "color": color_O},
+                {"value": 7, "color": color_AB}
+            ]
+        },
+        {
+            "type": "stacked_bar",
+            "key": "China",
+            "stacks": [
+                {"value": 3, "color": color_B},
+                {"value": 2, "color": color_AB},
+            ]
+        }
+    ],
+    "meta": {
+        "x_title": "Country",
+        "y_title": "Proportion"
     }
-]
+}
 
 function make_offset(stack){
     return stack.map((d, i, a) => {
@@ -43,35 +59,45 @@ function make_offset(stack){
     })
 }
 
-let svg_width = 500,
-    svg_height = 500,
-    margin = 30,
-    width = svg_width - 2 * margin,
-    height = svg_height - 2 * margin
+function render(spec){
+    let svg_width = 500,
+        svg_height = 500,
+        margin = 50,
+        width = svg_width - 2 * margin,
+        height = svg_height - 2 * margin
 
-let svg = d3.select("#barchart").attr("width", svg_width).attr("height", svg_height)
-let view = svg.append("g").attr("transform", translate(margin, margin))
+    let svg = d3.select("#barchart").attr("width", svg_width).attr("height", svg_height)
+    let view = svg.append("g").attr("transform", translate(margin, margin))
 
-let bar_heights = data.map(d => {
-    if(d.type === "stacked_bar"){
-        return d.stacks.reduce((a, b) => a + b.value, 0)
-    }
-    else return d.bar.value
-})
+    let bar_heights = spec.marks.map(d => {
+        if(d.type === "stacked_bar"){
+            return d.stacks.reduce((a, b) => a + b.value, 0)
+        }
+        else return d.bar.value
+    })
 
-console.log(bar_heights)
+    let x = d3.scaleBand().domain(spec.marks.map(d => d.key)).range([0, width]).padding(0.1)
+    let y = d3.scaleLinear().domain([0, d3.max(bar_heights)]).range([height, 0])
+    let x_axis = d3.axisBottom(x)
+    let y_axis = d3.axisLeft(y)
+    svg.append("g").attr("transform", translate(margin, margin + height)).call(x_axis)
+    svg.append("g").attr("transform", translate(margin, margin)).call(y_axis)
 
-let x = d3.scaleBand().domain(data.map((d, i) => i)).range([0, width]).padding(0.1)
-let y = d3.scaleLinear().domain([0, d3.max(bar_heights)]).range([0, height])
+    let x_label = svg.append("text").attr("transform", translate(svg_width / 2, margin + height + 2 * margin / 3)).style("text-anchor", "middle").text(spec.meta.x_title)
 
-let bars = view.selectAll("g").data(data)
-let stacks = bars.enter().append("g")
-        .attr("transform", (d, i) => translate(x(i), 0))
-    .selectAll("rect").data(d => d.type === "bar" ? make_offset([d.bar]) : make_offset(d.stacks))
-stacks.enter().append("rect")
-  .attr("width", x.bandwidth())
-  .attr("height", d => y(d.value))
-  .attr("transform", (d, i) => {
-      return translate(0, height - y(d.offset + d.value))
-  })
-  .style("fill", d => d.color)
+    let bars = view.selectAll("g").data(spec.marks)
+    let stacks = bars.enter().append("g")
+            .attr("transform", d => translate(x(d.key), 0))
+        .selectAll("rect").data(d => d.type === "bar" ? make_offset([d.bar]) : make_offset(d.stacks))
+    stacks.enter().append("rect")
+    .attr("width", x.bandwidth())
+    .attr("height", d => height - y(d.value))
+    .attr("transform", (d, i) => {
+        return translate(0, y(d.offset + d.value))
+    })
+    .style("fill", d => d.color)
+    .on("mouseover", function(){ d3.select(this).classed("focused", true) })
+    .on("mouseout", function(){ d3.select(this).classed("focused", false) })
+}
+
+render(barchart_spec)
