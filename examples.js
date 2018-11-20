b7256054f9380c51937be1891e0c05a91cfa6f92
javelinsman <javelinsman@snu.ac.kr>
2018-11-20 23:20:42 +0900

    spec ~ sorted1

--- examples.js
+++ examples.js
@@ -264,6 +264,49 @@ let stacked3 = {
     ]
 }
 
+let stacked4 = {
+    meta: {
+        title: "",
+        x_title: "",
+        y_title: "",
+        y_unit: "%",
+        colors: {
+            "Barley": "rgb(152, 153, 157)",
+            "Wheat": "rgb(116, 117, 119)",
+            "Rye": "rgb(209, 210, 212)"
+        }
+    },
+    marks: [
+        {
+            type: "stacked_bar",
+            key: "20X1",
+            stacks: [
+                {value: 12, color: {name: "Barley"}},
+                {value: 43, color: {name: "Wheat"}},
+                {value: 45, color: {name: "Rye"}},
+            ]
+        },
+        {
+            type: "stacked_bar",
+            key: "20X2",
+            stacks: [
+                {value: 7, color: {name: "Barley"}},
+                {value: 39, color: {name: "Wheat"}},
+                {value: 54, color: {name: "Rye"}},
+            ]
+        },
+        {
+            type: "stacked_bar",
+            key: "20X3",
+            stacks: [
+                {value: 7, color: {name: "Barley"}},
+                {value: 41, color: {name: "Wheat"}},
+                {value: 52, color: {name: "Rye"}},
+            ]
+        },
+    ]
+}
+
 let grouped1 = {
     meta: {
         title: "",
@@ -426,14 +469,141 @@ let grouped3 = {
     ]
 }
 
+let histogram1 = {
+    meta: {
+        title: "",
+        x_title: "",
+        y_title: "",
+        gridline: "grid",
+        width: 600,
+        height: 450,
+        orientation: "horizontal",
+        x_tick_rotate: "15",
+    },
+    marks: [
+        {
+            type: "bar",
+            key: "January",
+            bar: {value: 82000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "February",
+            bar: {value: 60000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "March",
+            bar: {value: 35000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "April",
+            bar: {value: 20000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "May",
+            bar: {value: 25000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "June",
+            bar: {value: 13000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "July",
+            bar: {value: 21000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "August",
+            bar: {value: 32000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "September",
+            bar: {value: 29000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "October",
+            bar: {value: 45000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "November",
+            bar: {value: 72000, color: "gold", label:{position:"top", format:"%v"}}
+        },
+        {
+            type: "bar",
+            key: "December",
+            bar: {value: 65000, color: "gold", label:{position:"top", format:"%v"}}
+        },
 
+    ]
+}
+
+let histogram2 = {
+    meta: {
+        title: "Pupil/Teacher Ratio in Primary Schools, January 1997",
+        x_title: "Pupil/Teacher Ratio",
+        y_title: "Percentage",
+        orientation: "horizontal",
+        x_tick_rotate: "45"
+    },
+    marks: [
+        { type: "bar", key: "Less than 16", bar: {value: 2, color: "skyblue"}, },
+        { type: "bar", key: "16", bar: {value: 1, color: "skyblue"}, },
+        { type: "bar", key: "17", bar: {value: 2, color: "skyblue"}, },
+        { type: "bar", key: "18", bar: {value: 3, color: "skyblue"}, },
+        { type: "bar", key: "19", bar: {value: 4, color: "skyblue"}, },
+        { type: "bar", key: "20", bar: {value: 6, color: "skyblue"}, },
+        { type: "bar", key: "21", bar: {value: 8, color: "skyblue"}, },
+        { type: "bar", key: "22", bar: {value: 11, color: "skyblue"}, },
+        { type: "bar", key: "23", bar: {value: 13, color: "skyblue"}, },
+        { type: "bar", key: "24", bar: {value: 14, color: "skyblue"}, },
+        { type: "bar", key: "25", bar: {value: 12, color: "skyblue"}, },
+        { type: "bar", key: "26", bar: {value: 5, color: "skyblue"}, },
+        { type: "bar", key: "Over 27", bar: {value: 4, color: "skyblue"}, },
+    ]
+}
+
+let sorted1 = {
+    meta: {
+        title: "All Incidents by Assignment",
+        x_title: "",
+        y_title: "Incidents",
+        orientation: "",
+        x_tick_rotate: "15",
+        gridline: "grid"
+    },
+    marks: [
+        { type: "bar", key: "ITIL UserV", bar: {value: 17, color: "skyblue"}, },
+        { type: "bar", key: "David Loo", bar: {value: 10, color: "darkgreen"}, },
+        { type: "bar", key: "Don Goodlife", bar: {value: 7, color: "yellow"}, },
+        { type: "bar", key: "Beth Anglin", bar: {value: 5, color: "darkblue"}, },
+        { type: "bar", key: "Howard Johnson", bar: {value: 3, color: "orange"}, },
+        { type: "bar", key: "Luke Wilson", bar: {value: 3, color: "purple"}, },
+        { type: "bar", key: "Bud Richman", bar: {value: 2, color: "blue"}, },
+        { type: "bar", key: "Charlie Whitherspoon", bar: {value: 2, color: "green"}, },
+        { type: "bar", key: "Bow Ruggeri", bar: {value: 1, color: "cyan"}, },
+        { type: "bar", key: "Fred Luddy", bar: {value: 1, color: "gray"}, },
+        { type: "bar", key: "Other", bar: {value: 1, color: "pink"}, },
+    ]
+}
 
 let example_specs = [
     {name: 'simple1', spec: simple1},
     {name: 'stacked1', spec: stacked1},
     {name: 'stacked2', spec: stacked2},
     {name: 'stacked3', spec: stacked3},
+    {name: 'stacked4', spec: stacked4},
     {name: 'grouped1', spec: grouped1},
     {name: 'grouped2', spec: grouped2},
     {name: 'grouped3', spec: grouped3},
+    {name: 'histogram1', spec: histogram1},
+    {name: 'histogram2', spec: histogram2},
+    {name: 'sorted1', spec: sorted1},
 ]
